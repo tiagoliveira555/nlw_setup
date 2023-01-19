6743822cb3d1eb1bed7c3ed6ed25881b9c518723
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-01-18 21:47:44 -0300

    routes /habits and /day

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { appRoutes } from './routes'
 
 const app = Fastify()
 
 app.register(cors)
-
-app.get('/', () => {
-  return 'Hello World'
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333
