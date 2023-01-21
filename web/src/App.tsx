562eb6f049e4b126a95455e1267373f37be7f1d0
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-01-21 17:39:43 -0300

    backend finished and web started

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,17 @@
-import './styles/global.css' 
+import './styles/global.css'
+
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
 export function App() {
 
   return (
-    <>
-      <h1 className='bg-zinc-300' >Testando tailwindcss</h1>
-    </>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
 
